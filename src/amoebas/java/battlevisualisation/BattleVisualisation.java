4dc9b677e1d248d2b852f11ea22a7f058a71a074
Maxim <mskurydin@gmail.com>
2010-04-12 16:26:58 +0300

    Battle visualisation. Initial commit

---
+++ src/amoebas/java/battlevisualisation/BattleVisualisation.java
@@ -0,0 +1,18 @@
+package amoebas.java.battlevisualisation;
+
+
+/**
+ * @author m 
+ */
+public class BattleVisualisation {
+
+	/**
+	 * Application's entry point.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Battle Visualisation");
+	}
+
+}
