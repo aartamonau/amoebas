ca4862fdbe24fcbb4e25c102e4adbf231e034a6c
Maxim Skurydin <m@sanctuary.u5>
2010-05-13 23:52:58 +0300

    Info displaying. Usage example: InfoDisplay.create(statePanel); InfoDisplay.instance().showGenerationNum(12); InfoDisplay.instance().showBestAmoeba(amoeba1); InfoDisplay.instance().showPopulationFitness(1.0); InfoDisplay.instance().showPopulationSize(100500);

--- src/amoebas/java/battlevisualisation/StatePanel.java
+++ src/amoebas/java/battlevisualisation/StatePanel.java
@@ -5,7 +5,8 @@ package amoebas.java.battlevisualisation;
 
 
 import java.awt.Color;
-
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -14,9 +15,53 @@ import javax.swing.JPanel;
  *
  */
 public class StatePanel extends JPanel {
-
+	
+	
+	public static final String GENERATION_NUM_TEXT = "Generation: ";
+	public static final String POPULATION_SIZE_TEXT = "Population Size : ";
+	public static final String FITNESS_DELTA_TEXT = "Population Fitness Delta: ";
+	public static final String BEST_AMOEBA_TEXT = "Best amoeba: ";
+	
+	
 	public StatePanel() {
-		setBackground(Color.GRAY);
+		
+		setLayout(new GridLayout(20, 1));
+		
+		setBackground(Color.WHITE);
+				
+		generationNumLabel = new JLabel(GENERATION_NUM_TEXT);
+		populationSizeLabel = new JLabel(POPULATION_SIZE_TEXT);
+		fitnessDeltaLabel = new JLabel(FITNESS_DELTA_TEXT);
+		bestAmoebaLabel = new JLabel(BEST_AMOEBA_TEXT);
+		
+		add(generationNumLabel);
+		add(populationSizeLabel);
+		add(fitnessDeltaLabel);
+		add(bestAmoebaLabel);	
+	}
+	
+	
+
+	public JLabel getGenerationNumLabel() {
+		return generationNumLabel;
+	}
+	
+	public JLabel getPopulationSizeLabel() {
+		return populationSizeLabel;
+	}
+	
+	public JLabel getFitnessDeltaLabel() {
+		return fitnessDeltaLabel;
+	}
+	
+	public JLabel getBestAmoebaLabel() {
+		return bestAmoebaLabel;
 	}
 
+
+
+	private JLabel generationNumLabel;
+	private JLabel populationSizeLabel;
+	private JLabel fitnessDeltaLabel;
+	private JLabel bestAmoebaLabel;
 }
