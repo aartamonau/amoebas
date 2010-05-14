b8e6889137ac779e787da9a46ea83df892a36a4c
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-05-14 05:04:40 +0300

    IBrain interface implementation. IMPORTANT: Point and Rectangle must be change to other classes that support floating point coordinates as all inputs and outputs of neural network are floating point numbers of the range (-1, 1).

---
+++ src/clojure/amoebas/brain.clj
@@ -0,0 +1,59 @@
+(ns amoebas.brain
+  (:use amoebas.neat.phenotype
+        amoebas.utils.seq)
+  (:import (java.awt Point)))
+
+(defn slice [[start end] xs]
+  (take (inc (- end start))
+        (drop start xs)))
+
+(defn make-point [x y]
+  (Point. x y))
+
+(def movement-outputs [0 1])
+(def aim-outputs      [2 3])
+(def shoot-output     4)
+
+(defn make-brain [neural-net]
+  (let [nn        (atom neural-net)
+        reactions (atom nil)]
+    (proxy [amoebas.java.battleSimulation.IBrain] []
+
+      (feedSenses
+         [self-position enemy-position thorn-position walls]
+         (do (swap! nn (fn [nn]
+                         (update nn
+                                 (concat
+                                  [ (. self-position x)
+                                    (. self-position y)
+                                    (. enemy-position x)
+                                    (. enemy-position y) ]
+
+                                  (if (nil? thorn-position)
+                                    [-1.0, -1.0]
+                                    [ (. thorn-position x)
+                                      (. thorn-position y)])
+
+                                  (concat-map #(vector (. % x)
+                                                       (. % y)
+                                                       (. % width)
+                                                       (. % height))
+                                              (seq walls))))))
+             (reset! reactions (outputs @nn))))
+
+      (getMovementVector
+         []
+         (apply make-point
+                (slice movement-outputs @reactions)))
+
+      (getAimVector
+         []
+         (apply make-point
+                (slice aim-outputs @reactions)))
+
+      (shallWeShoot
+         []
+         (let [reaction (nth @reactions shoot-output)]
+           (if (> reaction 0)
+             true
+             false))))))
